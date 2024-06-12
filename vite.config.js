6d7139cc6334c563375028da7fe39aade15076d7
TunaKadirBerkay <Kadir.Tuna@Student.HTW-Berlin.de>
2024-06-12 14:09:29 +0200

    env und vite config aktuel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import {defineConfig} from "vite";
-
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve:
-    {
-      alias: {
-        '@': require('path').resolve(__dirname, './src')
-      }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': require('path').resolve(__dirname, './src')
+        }
+    },
+    server: {
+        port: 5180
     }
-})
+});
